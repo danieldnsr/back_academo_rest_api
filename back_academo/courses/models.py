6af86b991eca642738316d87cac053f1fe5710cc
danieldnsr <danieldnsr@gmail.com>
2022-10-13 23:36:43 -0500

    Created the itembase abstract model and the video model to storage the content of each class, created the orderfield that inherits from positiveintegerfield django module and is use to assign order to the classes objects.

--- back_academo/courses/models.py
+++ back_academo/courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 class Subject(models.Model):
@@ -30,3 +32,24 @@ class Classes(models.Model):
 
     def __str__(self):
         return self.title
+
+class Content(models.Model):
+    module = models.ForeignKey(Classes, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE, limit_choices_to={'model__in' : ('video')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+class ItemBase(models.Model):
+    
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+    def __str__(self):
+        return self.title
+
+class Video(ItemBase):
+    url = models.URLField()
