c1e85087bf3a946046c9df66a60bee6543351f34
danieldnsr <danieldnsr@gmail.com>
2022-10-15 00:20:32 -0500

    Created custom fields inherit from PositiveintegerFiled that allows to enumerate the classes in order of creation. Create the configuration for the views.

--- back_academo/back_academo/settings.py
+++ back_academo/back_academo/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     #PROJECT APPS
     'courses.apps.CoursesConfig',
     #THIRD PARTY APPS
-
+    'rest_framework',
     #DEFAULT APPS
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.jangoModelPermissionsOrAnonReadOnly'
+    ]
+}
