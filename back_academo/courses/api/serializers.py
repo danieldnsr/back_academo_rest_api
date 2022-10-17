56a1d7e24328d396f576d16d0884a27f8c4f63e6
danieldnsr <danieldnsr@gmail.com>
2022-10-17 15:14:18 -0500

    Create the api views for enrollment users.

--- back_academo/courses/api/serializers.py
+++ back_academo/courses/api/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from ..models import Subject
+from ..models import Course, Classes
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title']
+    
+class ClassesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Classes
+        fields = ['order', 'title', 'description']
+
+class CourseSerializer(serializers.ModelSerializer):
+    classes = ClassesSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'overview', 'created', 
+                    'owner', 'classes']
